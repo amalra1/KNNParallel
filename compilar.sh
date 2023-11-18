5d1d2d58eb6dee2cb7791eb58031feb88da43c49
Pedro <amaral1@ufpr.br>
2023-11-18 17:16:02 -0300

    Add a flag -O3

--- compilar.sh
+++ compilar.sh
@@ -2,6 +2,6 @@
 
    source /home/software/psxe_2020/parallel_studio_xe_2020.0.088/psxevars.sh
 
-   mpicc knn-mpi.c chrono.c -o knn-mpi -lm
+   mpicc -O3 knn-mpi.c chrono.c -o knn-mpi -lm
 
 echo "  **** COMPILACAO COMPLETA ****  " 
